e4ce60e0fb116c089df0cfa031e2abf9ee1ff51c
greeneeya <suzythetravelerr@gmail.com>
2023-06-29 15:15:13 +0900

    darkmode context updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { HiSun, HiMoon } from 'react-icons/hi';
-import { DarkModeContext } from '../context/darkModeContext';
+import { useDarkModeContext } from '../context/darkModeContext';
 import cls from 'classnames';
 
 export default function Header({ filters, filter, setFilter }) {
-  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const { darkMode, toggleDarkMode } = useDarkModeContext();
 
   return (
     <header className="flex items-center justify-between p-4 rounded-t bg-gray-300">
       <button
         type="button"
-        className={cls(
-          'text-lg cursor-pointer hover:text-gray-200',
-          { 'text-blue-400 ': darkMode },
-          { 'text-orange-600': !darkMode }
-        )}
+        className="text-orange-600 dark:text-blue-500 text-lg cursor-pointer hover:rotate-12 hover:scale-125"
         onClick={toggleDarkMode}
       >
         {darkMode ? <HiMoon /> : <HiSun />}
